7f8cbe494ff38dd06744cd2eb138d2332be8fa74
GomesMarcos <gomes.marcosjf@gmail.com>
2021-10-23 14:02:34 -0300

    get interval recursively

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,16 @@ import Header from './components/Header'
 import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
 import GameScreen from './screens/GameScreen'
+import axios from 'axios'
 
 function App() {
+  const user_settings = get_user_settings('123jcoij23')
   return (
     <BrowserRouter className="App">
       <Header />
       <main className="py-3">
         <Container>
-          <GameScreen user_id="123jcoij23" />
+          <GameScreen user_settings={user_settings} />
           <Route path="/" component={HomeScreen} />
           <Route path="/" />
           <Route path="/" />
@@ -23,3 +25,17 @@ function App() {
 }
 
 export default App
+
+async function get_user_settings(user_id) {
+  const user = await get_user(user_id)
+  const settings = await get_settings(user.data.settings)
+  return settings
+}
+
+function get_settings(settings_id) {
+  return axios.get(`/settings/${settings_id}/`)
+}
+
+function get_user(user_id) {
+  return axios.get(`/users/${user_id}/`)
+}
--- frontend/src/screens/GameScreen.js
+++ frontend/src/screens/GameScreen.js
@@ -17,17 +17,14 @@ import { EASY_INTERVALS, MEDIUM_INTERVALS, HARD_INTERVALS, NOTES } from '../cons
 
 import Octave from '../components/Octave'
 
-function GameScreen({ user_id }) {
-  const user_settings = get_user_settings(user_id)
-  console.log(user_settings)
-  const setted_intervals = get_intervals_by_difficulty(user_settings.data.difficulty)
-  console.log(setted_intervals)
+function GameScreen({ user_settings }) {
+  const setted_intervals = get_intervals_by_difficulty(user_settings.difficulty)
+  const interval = get_interval(setted_intervals, user_settings.direction)
 
   //create a synth and connect it to the main output (your speakers)
   const synth = new Tone.Synth().toDestination()
 
   //play a middle 'C' for the duration of an 8th note
-  const interval = get_interval(NOTES)
   console.log(interval)
   console.log(`${NOTES[interval[0]]}`, `${NOTES[interval[1]]}`)
   // synth.triggerAttackRelease(`${NOTES[interval[0]]}`, '8n')
@@ -45,62 +42,38 @@ function GameScreen({ user_id }) {
 }
 export default GameScreen
 
-function get_interval(NOTES, direction = null) {
-  switch (direction) {
-    // Ascendent interval
-    case 1:
-      return get_ascendent_interval(NOTES)
+function get_interval(settings_intervals, direction = undefined) {
+  // Set interval
+  let interval = set_interval()
 
-    // Descendent interval
-    case 2:
-      return get_descendent_interval(NOTES)
-
-    default:
-      const interval = []
-      interval.push(Math.floor(Math.random() * NOTES.length))
-      interval.push(Math.floor(Math.random() * NOTES.length))
-
-      return interval
-  }
+  // Retunr validated interval
+  return validate_interval(interval, direction, settings_intervals)
 }
 
-function get_ascendent_interval(NOTES) {
-  const interval = []
-  interval[0] = Math.floor(Math.random() * NOTES.length)
-  let random = Math.floor(Math.random() * NOTES.length)
+function set_interval() {
+  return [Math.floor(Math.random() * NOTES.length), Math.floor(Math.random() * NOTES.length)]
+}
 
-  if (random >= interval[0]) {
-    interval.push(random)
+function validate_interval(interval, direction, settings_intervals) {
+  const subtracted_interval = Math.abs(interval[0] - interval[1])
+  const is_interval_in_settings_intervals = settings_intervals[subtracted_interval] != undefined
+
+  // If Ascendent Interval
+  if (direction === 1 && interval[0] <= interval[1] && is_interval_in_settings_intervals) {
     return interval
   }
 
-  return get_ascendent_interval(NOTES)
-}
-function get_descendent_interval(NOTES) {
-  const interval = []
-  interval[0] = Math.floor(Math.random() * NOTES.length)
-  let random = Math.floor(Math.random() * NOTES.length)
-
-  if (random <= interval[0]) {
-    interval.push(random)
+  // If Descendent Interval
+  if (direction === 2 && interval[0] >= interval[1] && is_interval_in_settings_intervals) {
     return interval
   }
 
-  return get_descendent_interval(NOTES)
-}
+  // If Directions not matters
+  if (is_interval_in_settings_intervals) {
+    return interval
+  }
 
-async function get_user_settings(user_id) {
-  const user = await get_user(user_id)
-  const settings = await get_settings(user.data.settings)
-  return user, settings
-}
-
-function get_settings(settings_id) {
-  return axios.get(`/settings/${settings_id}/`)
-}
-
-function get_user(user_id) {
-  return axios.get(`/users/${user_id}/`)
+  return validate_interval(set_interval(), direction, settings_intervals)
 }
 
 function get_intervals_by_difficulty(difficulty) {
