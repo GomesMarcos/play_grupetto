463a06174368a4760781ff6e844d2b89a4783f22
GomesMarcos <gomes.marcosjf@gmail.com>
2021-10-25 01:38:55 -0300

    Change playing DOM class with useState

--- frontend/src/components/Key.js
+++ frontend/src/components/Key.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Key({ note, is_playing, first_note, last_note }) {
   const key_class = note.includes('#') ? 'sus' : 'natural',
-    is_playing_class = is_playing ? ' is-playing' : '',
     first = first_note.includes(note) ? ' first' : '',
     last = last_note.includes(note) ? ' second' : ''
-  return (
-    <div className={`key ${key_class}${is_playing_class}${first}${last}`} id={first || last}></div>
-  )
+
+  const [isPlayingClass, setIsPlayingClass] = useState(is_playing ? ' is-playing' : '')
+
+  console.log(isPlayingClass)
+
+  setTimeout(() => {
+    setIsPlayingClass('')
+  }, 1000)
+
+  return <div className={`key ${key_class}${isPlayingClass}${first}${last}`}></div>
 }
 
 export default Key
